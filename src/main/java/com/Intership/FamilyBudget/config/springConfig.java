fd70644785c36704097fdcc6041f7e73505eeaf3
Yaroslav Luchinskii <luchicyaric@gmail.com>
2023-04-11 20:44:57 +0300

    added custom spring config file

--- src/main/java/com/Intership/FamilyBudget/config/springConfig.java
+++ src/main/java/com/Intership/FamilyBudget/config/springConfig.java
@@ -2,9 +2,18 @@ package com.Intership.FamilyBudget.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 
 @Configuration
 public class springConfig implements WebMvcConfigurer {
@@ -13,10 +22,31 @@ public class springConfig implements WebMvcConfigurer {
         return new RestTemplate();
     }
 
+    @Bean
+    public MultipartResolver multipartResolver() {
+        CommonsMultipartResolver multipartResolver = new CommonsMultipartResolver();
+        multipartResolver.setMaxUploadSize(5242880);
+        return multipartResolver;
+    }
+
+//    @Override
+//    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+//        MappingJackson2HttpMessageConverter jacksonConverter = new MappingJackson2HttpMessageConverter();
+//        converters.add(jacksonConverter);
+//
+//        ByteArrayHttpMessageConverter byteArrayConverter = new ByteArrayHttpMessageConverter();
+//        converters.add(byteArrayConverter);
+//
+//        FormHttpMessageConverter formConverter = new FormHttpMessageConverter();
+//        formConverter.setCharset(StandardCharsets.UTF_8);
+//        formConverter.setMultipartCharset(StandardCharsets.UTF_8);
+//        converters.add(formConverter);
+//    }
+
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
                 .allowedOrigins("http://localhost:8080")
-                .allowedMethods("GET", "POST", "PUT", "DELETE");
+                .allowedMethods("GET", "POST", "PUT", "DELETE","PATCH");
     }
 }
