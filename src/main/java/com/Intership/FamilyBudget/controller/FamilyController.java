57180d7fe06cf0c0714327116a04938158610974
Yaroslav Luchinskii <luchicyaric@gmail.com>
2023-04-11 20:46:22 +0300

    added new methods to controllers

--- src/main/java/com/Intership/FamilyBudget/controller/FamilyController.java
+++ src/main/java/com/Intership/FamilyBudget/controller/FamilyController.java
@@ -1,12 +1,22 @@
 package com.Intership.FamilyBudget.controller;
 
+import com.Intership.FamilyBudget.dto.*;
+import com.Intership.FamilyBudget.model.Family;
+import com.Intership.FamilyBudget.model.User;
+import com.Intership.FamilyBudget.model.role.Role;
 import com.Intership.FamilyBudget.service.FamilyService;
 import com.Intership.FamilyBudget.service.UserService;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
-@RequestMapping("/family")
+@RequestMapping("/families")
 public class FamilyController {
 
     private final FamilyService familyService;
@@ -17,5 +27,70 @@ public class FamilyController {
         this.userService = userService;
     }
 
+    @PostMapping({"/create/family"})
+    public ResponseEntity<FamilyResponseDTO> create(@RequestBody FamilyRequestDTO familyRequestDTO){
+        System.out.println(familyRequestDTO);
+        Family status = familyService.getAll().stream().filter(family -> family.getName().equals(familyRequestDTO.getName()))
+                .findFirst()
+                .orElse(null);
+        if (status==null){
+            Family family = new Family();
+            family.setName(familyRequestDTO.getName());
+            family.setBudget(familyRequestDTO.getBudget());
+            family.setUsers(new ArrayList<>());
+            familyService.create(family);
+            return new ResponseEntity<>(new FamilyResponseDTO(family), HttpStatus.OK);
+        }else {
+            return new ResponseEntity<>(new FamilyResponseDTO(status), HttpStatus.BAD_REQUEST);
+        }
+    }
 
+    @PatchMapping({"/update/family/{id}"})
+    public ResponseEntity<FamilyResponseDTO> update(@PathVariable(value = "id") int id,
+                                                  @RequestBody FamilyRequestDTO familyRequestDTO) throws IOException {
+      if (familyRequestDTO!=null){
+          Family family = new Family();
+          family.setId(id);
+          family.setName(familyRequestDTO.getName());
+          family.setBudget(familyRequestDTO.getBudget());
+          familyService.update(family);
+            return new ResponseEntity<>(new FamilyResponseDTO(family), HttpStatus.OK);
+        }
+        else {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @GetMapping
+    public ResponseEntity<List<FamilyResponseDTO>> findFamily(){
+        List<FamilyResponseDTO> families = familyService.getAll().stream()
+                .map(FamilyResponseDTO::new).toList();
+
+        if (families.isEmpty()) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(families, HttpStatus.OK);
+
+    }
+
+    @GetMapping({"/family/check/budget/{family_id}"})
+    public ResponseEntity<CheckBudgetDTO> checkFamilyBudget(@PathVariable(value = "family_id") int family_id){
+        Family family = familyService.readById(family_id);
+        if (family!=null) {
+            if (familyService.checkBudget(family.getBudget(), family.getActualBudget())) {
+                return new ResponseEntity<>(new CheckBudgetDTO(true, "Увага! Залишилось менше 10% бюджету на місяць"), HttpStatus.OK);
+            } else return new ResponseEntity<>(new CheckBudgetDTO(false, ""), HttpStatus.OK);
+        }else return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @GetMapping({"/family/{family_id}"})
+    public ResponseEntity<FamilyResponseDTO> getFamilyById(@PathVariable(value = "family_id") int family_id){
+        Family family = familyService.readById(family_id);
+        return new ResponseEntity<>(new FamilyResponseDTO(family), HttpStatus.OK);
+    }
+
+    @DeleteMapping({"/family/delete/{id}"})
+    public void delete(@PathVariable(value = "id") int id){
+        familyService.delete(id);
+    }
 }
--- src/main/java/com/Intership/FamilyBudget/controller/UserController.java
+++ src/main/java/com/Intership/FamilyBudget/controller/UserController.java
@@ -1,14 +1,23 @@
 package com.Intership.FamilyBudget.controller;
 
-import com.Intership.FamilyBudget.dto.UserRequestDTO;
-import com.Intership.FamilyBudget.dto.UserResponseDTO;
+import com.Intership.FamilyBudget.dto.*;
 import com.Intership.FamilyBudget.model.User;
 import com.Intership.FamilyBudget.model.role.Role;
 import com.Intership.FamilyBudget.service.RoleService;
+import com.Intership.FamilyBudget.service.ShoppingHistoryService;
 import com.Intership.FamilyBudget.service.UserService;
+import net.bytebuddy.asm.Advice;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -19,9 +28,12 @@ public class UserController {
     private final UserService userService;
     private final RoleService roleService;
 
-    public UserController(UserService userService, RoleService roleService) {
+    private final ShoppingHistoryService shoppingHistoryService;
+
+    public UserController(UserService userService, RoleService roleService, ShoppingHistoryService shoppingHistoryService) {
         this.userService = userService;
         this.roleService = roleService;
+        this.shoppingHistoryService = shoppingHistoryService;
     }
 
     @PostMapping({"/create/user"})
@@ -34,11 +46,16 @@ public class UserController {
             user.setName(userRequestDTO.getName());
             user.setLastName(userRequestDTO.getLastName());
             user.setSurName(userRequestDTO.getSurName());
+            user.setAddress(userRequestDTO.getAddress());
+            user.setPhoneNumber(userRequestDTO.getPhoneNumber());
             user.setBudget(0);
             user.setEmail(userRequestDTO.getEmail());
                 Role role = roleService.readById(1);
             user.setRole(role);
+            byte[] img = new byte[4];
+            user.setProfileImage(img);
             user.setPassword(userRequestDTO.getPassword());
+            user.setPostalIndex(userRequestDTO.getPostalIndex());
             userService.create(user);
             return new ResponseEntity<>(new UserResponseDTO(user), HttpStatus.OK);
         }else {
@@ -46,25 +63,41 @@ public class UserController {
         }
     }
 
-
     @PatchMapping({"/update/user/{id}"})
     public ResponseEntity<UserResponseDTO> update(@PathVariable(value = "id") int id,
-                                                  @RequestBody UserRequestDTO userRequestDTO){
+                                                  @RequestBody UserRequestDTO userRequestDTO) throws IOException {
         if (userRequestDTO!=null){
+            System.out.println(userRequestDTO);
+            System.out.println("ID is:"+id);
             User user = new User();
             user.setId(id);
             user.setName(userRequestDTO.getName());
             user.setLastName(userRequestDTO.getLastName());
             user.setSurName(userRequestDTO.getSurName());
-            user.setBudget(userRequestDTO.getBudget());
+            user.setAddress(userRequestDTO.getAddress());
+            user.setPhoneNumber(userRequestDTO.getPhoneNumber());
             user.setPassword(userRequestDTO.getPassword());
             user.setEmail(userRequestDTO.getEmail());
             user.setRole(roleService.readById(1));
+            user.setFamily(userService.readById(id).getFamily());
+            user.setPostalIndex(userRequestDTO.getPostalIndex());
+            user.setProfileImage(new byte[4]);
             userService.update(user);
             return new ResponseEntity<>(new UserResponseDTO(user), HttpStatus.OK);
         }
         else {
-             throw new NullPointerException();
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+    }
+
+    @PostMapping({"/check_data"})
+    public ResponseEntity<ErrorResponseDTO> checkError(@RequestBody @Valid UserRequestDTO userRequestDTO, BindingResult bindingResult){
+        if (bindingResult.hasErrors()) {
+            ErrorResponseDTO errorResponseDTO = new ErrorResponseDTO("Validation failed. " +
+                    "Please check the request parameters and try again.", bindingResult.getFieldErrors());
+            return ResponseEntity.badRequest().body(errorResponseDTO);
+        }else {
+            return new ResponseEntity<>(HttpStatus.OK);
         }
     }
 
@@ -74,42 +107,12 @@ public class UserController {
         return new ResponseEntity<>(new UserResponseDTO(user), HttpStatus.OK);
     }
 
-    @GetMapping
-    public ResponseEntity<List<UserResponseDTO>> findUser(@RequestParam(value = "family_id") int family_id,
-                                                         @RequestParam(value = "isOrdered") boolean isOrdered, @RequestParam(value = "name") String name,
-                                                         @RequestParam(value = "surName") String surName, @RequestParam(value = "lastName") String lastName){
-        List<UserResponseDTO> users;
+    @GetMapping({"/inFamily/{family_id}"})
+    public ResponseEntity<List<UserResponseDTO>> findUser(@PathVariable(value = "family_id") int family_id){
+        List<UserResponseDTO> users = new ArrayList<>();
             if(family_id != 0) {
-                if (isOrdered) {
-                    users = userService.getAllOrderedByBudget(family_id).stream()
-                            .map(UserResponseDTO::new).collect(Collectors.toList());
-                } else {
                     users = userService.getAllByFamilyId(family_id).stream()
                             .map(UserResponseDTO::new).collect(Collectors.toList());
-                }
-            }
-            else if (name!=null && surName==null && lastName==null){
-                users = userService.readByName(name).stream()
-                        .map(UserResponseDTO::new).collect(Collectors.toList());
-            }
-            else if (name==null && surName!=null && lastName==null){
-                users = userService.readByLastName(surName).stream()
-                        .map(UserResponseDTO::new).collect(Collectors.toList());
-            }
-            else if (name==null && surName==null && lastName!=null){
-                users = userService.readByThirdName(lastName).stream()
-                        .map(UserResponseDTO::new).collect(Collectors.toList());
-            }
-            else if (name!=null && surName!=null && lastName==null){
-                users = userService.readByNameAndSurName(name, surName).stream()
-                        .map(UserResponseDTO::new).collect(Collectors.toList());
-            }
-            else if (name == null && surName != null){
-                users = userService.readBySurNameAndLastName(surName, lastName).stream()
-                        .map(UserResponseDTO::new).collect(Collectors.toList());
-            }else {
-                users = userService.getAll().stream()
-                        .map(UserResponseDTO::new).collect(Collectors.toList());
             }
         if (users.isEmpty()) {
             return new ResponseEntity<>(HttpStatus.NOT_FOUND);
@@ -122,8 +125,28 @@ public class UserController {
     public void delete(@PathVariable(value = "id") int id){
         userService.delete(id);
     }
+    @PostMapping({"/user/{user_id}/remover_from_family"})
+    public void removerUserFromFamily(@PathVariable(value = "user_id") int id){
+        userService.removeUserFromThisFamily(id);
+    }
 
+    @GetMapping({"/user/{user_id}/spending"})
+    public ResponseEntity<List<SpendPerDayDTO>> getMySpending(@PathVariable(value = "user_id") int id){
+        List<SpendPerDayDTO> spendPerDayDTOs = shoppingHistoryService.getUsersSpendingPerDayStatistic(id);
+        if (spendPerDayDTOs.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(spendPerDayDTOs,HttpStatus.OK);
+    }
 
-
-
+    @GetMapping({"/{family_id}/spending"})
+    public ResponseEntity<List<SortedUsersBySpendingDTO>> getUsersAndSpending(@PathVariable(value = "family_id") int id){
+        LocalDate startDate = LocalDate.now();
+        LocalDate endDate = LocalDate.now().minusDays(10);
+        List<SortedUsersBySpendingDTO> spendPerDayDTOs = userService.getSortedUsersBySpending(startDate,endDate,id);
+        if (spendPerDayDTOs.isEmpty()){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+        return new ResponseEntity<>(spendPerDayDTOs,HttpStatus.OK);
+    }
 }
